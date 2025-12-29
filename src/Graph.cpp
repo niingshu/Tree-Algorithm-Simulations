4a97faf88b3f279256c1aa4368d0864ee816e952
nshu15 <nshu15@students.ubc.ca>
2025-12-29 14:42:53 -0800

    implement graph source

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -9,14 +9,30 @@ Graph::Graph(int vertices, bool directed)
     }
 }
 
-//add an edge between u and v 
+//add an edge between u and v, directed then u->v, undirected add also from v to u
 void Graph::addEdge(int u, int v) { 
+    if (u < 0 || v < 0 || u > getSize() || v > getSize()) {
+        throw out_of_range("Vertex input out of range. Please revise");
+    }
+
+    adjList[u].push_back(v); //push v back as adjacent vertex with v
+
+    //undirected 
+    if (isDirected == 0) {
+        adjList[v].push_back(u);
+    }
+
 }
 
 const vector<int>& Graph::getNeigbours(int u) const {
+    if (u < 0 || u > getSize()) {
+        throw out_of_range("Vertex input out of range. Please revise");
+    }
 
+    return adjList[u];
 }
 
+//size of the graph
 int Graph::getSize() const {
-
+    return adjList.size();
 }
