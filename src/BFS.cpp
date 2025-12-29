ef7472db229f35c0dbb79dfa3fb4aeaa4bbfa940
nshu15 <nshu15@students.ubc.ca>
2025-12-29 15:15:37 -0800

    implement bfs course

---
+++ src/BFS.cpp
@@ -0,0 +1,35 @@
+#include "BFS.h"
+#include <queue>
+
+//constructor 
+BFS::BFS() {}; 
+
+//runs and returns the vector of discovery tree 
+vector<int> BFS::traverse(const Graph& graph, int start) {
+    if (start < 0 || start >= graph.getSize()) 
+        throw out_of_range("Vertex input out of range. Please revise");
+
+    //clear the list for visited and discory if traverse is being called multiple times 
+    visited.clear();
+    visited.assign(graph.getSize(), false);
+    discovery.clear();
+
+    queue<int> q; 
+    visited[start] = true; //marked as visited
+    discovery.push_back(start);
+    q.push(start);
+
+    while (!q.empty()) { //while the queue is not empty
+        for (auto neigh: graph.getNeigbours(q.front())) {  //get the neighbour in the front 
+            if (visited[neigh] == false) { //if hasnt been visited
+            visited[neigh] = true; 
+            discovery.push_back(neigh); //discovery tree
+            q.push(neigh);
+            }
+        }
+        q.pop();
+    }
+
+    return discovery;
+
+}
