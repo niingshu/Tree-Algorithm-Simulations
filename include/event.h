ebf85535bd31ee61f99c95d383ae395d435a9f56
nshu15 <nshu15@students.ubc.ca>
2026-01-19 15:06:47 -0800

    implemented event.h for db

---
+++ include/event.h
@@ -0,0 +1,25 @@
+#pragma once //make sure that the file is included only once
+#include <vector>
+
+enum class Action {
+    VISIT_NODE = 1;
+    DISCOVERY_EDGE = 2;
+    ADD_TO_TREE = 3;
+    RELAX_EDGE = 4;
+}
+
+struct Event {
+    Action action;
+    int u;
+    int v;
+    int weight = 0; //stored as default
+}
+
+class EventRecord {
+    public:
+    vector<Event> events;
+
+    void record(Action action, int u = -1, int v = -1, int weight = 0) {
+        events.push_back({action, u, v});
+    }
+}
